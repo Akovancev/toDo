c1d2d0a23f723445babab28a557082af01b7c089
Akovancev <apm120700@gmail.com>
2021-06-30 21:44:43 +0300

    Primitive addition of elements

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      newItem: '',
+      allItems: []
+    }
+
+    this.handleChange = this.handleChange.bind(this)
+    this.addItem = this.addItem.bind(this)
+  }
+
+  handleChange(e) {
+    this.setState({
+      newItem: e.target.value
+    })
+  }
+
+  addItem(event) {
+    if (event.code == 'Enter') {
+      const oldItems = this.state.allItems
+      const oldItem = this.state.newItem
+      this.setState({
+          newItem: '',
+          allItems: [{ name: oldItem}, ...oldItems]
+      })
+      debugger
+    }
+  }
+
+  componentDidMount() {
+    document.addEventListener('keydown', this.addItem)
+  }
+
+  render() {
+    return (
+      <div className='App'>
+        <input value={this.state.newItem} onChange={this.handleChange}/>
+        {this.state.allItems.map((item) => {
+          return (
+            <div>
+              {item.name}
+            </div>
+          )
+        })}
+      </div>
+    );
+  }
 }
 
 export default App;
